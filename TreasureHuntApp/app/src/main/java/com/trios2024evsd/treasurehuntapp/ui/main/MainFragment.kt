0f0b541b342094a079ad33efd79fe23ef79df2b8
sumana-hub <sumana.dhital@triosstudent.com>
2024-08-21 22:16:41 -0400

    Initial Commit

---
+++ TreasureHuntApp/app/src/main/java/com/trios2024evsd/treasurehuntapp/ui/main/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.trios2024evsd.treasurehuntapp.ui.main
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.trios2024evsd.treasurehuntapp.R
+import com.trios2024evsd.treasurehuntapp.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
